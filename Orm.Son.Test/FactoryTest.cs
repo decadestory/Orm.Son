492394b0386465ead99648f9381030d083266680
decadestory <jerry.core@foxmail.com>
2017-11-26 16:25:47 +0800

    .

--- Orm.Son.Test/FactoryTest.cs
+++ Orm.Son.Test/FactoryTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Orm.Son.Core;
 using Orm.Son.Test.Entities;
+using System.Threading.Tasks;
 
 namespace Orm.Son.Test
 {
@@ -35,5 +36,41 @@ namespace Orm.Son.Test
 
             Assert.IsTrue(true);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+           for(var i=0; i<100;i++)
+            {
+                var obj2 = new Demo
+                {
+                    Name = "JerryDemoMany2",
+                    Age = 10,
+                    Score = 56,
+                    AddTime = DateTime.Now
+                };
+                SonFact.Cur.Insert(obj2);
+            }
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Parallel.For(0,10000,t=> {
+                var obj2 = new Demo
+                {
+                    Name = "JerryDemoMany2",
+                    Age = 10,
+                    Score = 56,
+                    AddTime = DateTime.Now
+                };
+                SonFact.Cur.Insert(obj2);
+            });
+
+            Assert.IsTrue(true);
+        }
+
     }
 }
